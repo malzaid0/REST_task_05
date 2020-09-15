8a48913c9899d987ab66de71c09e98f9856cfea7
Mohammed Alzaid <m.al-zaid@live.com>
2020-09-15 16:01:42 +0300

    done

--- flights/serializers.py
+++ flights/serializers.py
@@ -5,31 +5,38 @@ from .models import Flight, Booking
 
 
 class FlightSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Flight
-		fields = ['destination', 'time', 'price', 'id']
+    class Meta:
+        model = Flight
+        fields = ['destination', 'time', 'price', 'id']
 
 
 class BookingSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Booking
-		fields = ['flight', 'date', 'id']
+    class Meta:
+        model = Booking
+        fields = ['flight', 'date', 'id']
 
 
 class BookingDetailsSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Booking
-		fields = ['flight', 'date', 'passengers', 'id']
+    class Meta:
+        model = Booking
+        fields = ['flight', 'date', 'passengers', 'id']
 
 
 class UpdateBookingSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Booking
-		fields = ['date', 'passengers']
+    class Meta:
+        model = Booking
+        fields = ['passengers']
+
+
+class AdminUpdateBookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['date', 'passengers']
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['username', 'password', 'first_name', 'last_name']
